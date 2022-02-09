96f5cde08b8622acd8b1e2e47cef7ed50c10a083
FRancoTol <toledofranco@rocketmail.com>
2022-02-09 09:14:31 -0300

    pimpon

--- 02 - Variables y Tipos de Datos/pinp.py
+++ 02 - Variables y Tipos de Datos/pinp.py
@@ -1,2 +1,69 @@
-pomp = "tres"
-print(pomp)
+vari_num = 34
+
+print(vari_num)   
+
+type(8.5)
+
+type(vari_num)
+
+vari_nom = "Hugo Franco Toledo"
+
+vari_com = 5j
+
+type(vari_com)
+
+pi_num = 3.1416
+
+var_t1 = "True"
+var_t2 = True
+
+print(type(var_t1), type(var_t2)) 
+
+var_combi = 2 + 2.4
+
+a = 4 + 4j
+b = 3 - 5j
+print(a + b)
+
+print(a + vari_num)
+
+r = vari_num * 5
+print(r)
+
+print(2 ** 8)
+
+var_cos = 27 / 4
+print(var_cos)
+
+print(27 // 4)
+print(27 % 4)
+
+print(6 * 4 + 3)
+
+var_str = "machote"
+print(vari_nom + var_str*2)
+
+dos = 2
+dosd = "2"
+print(dos == dosd)
+print(type(dos), type(dosd))
+
+print(int(dosd) + dos)
+
+print(dos == int(dosd))
+ 
+gtr = 3
+gtr -= 1
+print(gtr)
+
+print(1 << 2)
+
+print(float(dos) + float(dosd))
+print(int(dos) + int(dosd))
+print(str(dos) + str(dosd))
+
+print(var_str + str(gtr) * gtr)
+
+
+
+
