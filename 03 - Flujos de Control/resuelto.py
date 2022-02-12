f72a3a00d455443bcd629194c3fdd76edb33d6be
FRancoTol <toledofranco@rocketmail.com>
2022-02-12 17:03:09 -0300

    periplo

--- 03 - Flujos de Control/resuelto.py
+++ 03 - Flujos de Control/resuelto.py
@@ -1,21 +1,2 @@
-dip = 25
-if (dip > 0):
-        print("el resultado es mayor a cero")
-elif (dip < 0):
-        print("so boludo")
-else:
-        print("kctjyhxsrhty")
 
-min = 34
-don = "36"
-if (type(min) == type(don)):
-    print("puede ser")
-else:
-    print("tambien puede ser")
-
-for pipo in range(1,21):
-    print(pipo)
-
-for po in range(0,6):
-    print("elevamos",str(po), "a la tercera:",str(po**3))
     
