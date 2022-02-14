fec396a1ad504b21b6155d1f526bbbd56922e909
FRancoTol <toledofranco@rocketmail.com>
2022-02-14 16:40:25 -0300

    listillo

--- 03 - Flujos de Control/resuelto.py
+++ 03 - Flujos de Control/resuelto.py
@@ -1,47 +1,33 @@
+firp = 4
+tip = 1
+while firp > 0:
+        tip = tip * firp       
+        firp -= 1
+print(tip)
 
-from ast import Break
+listonaza = ["berenjenas", 3, "pan rayado", 1,  "papa", 3,"leche", "sal"]
+print(listonaza)
+print(listonaza[3:6])
+print(listonaza[:6])
+print(listonaza[4:])
+listonaza.append("pan")
+print(listonaza)
+listonaza.insert(3, "chokmpi")
+print(listonaza)
+listonaza.extend(["pelona", "pilcha", "manzana"])
+print(listonaza)
+print(listonaza.index("leche"))
+listonaza.remove("pan")
+print(listonaza)
+print(listonaza.pop())
+print(listonaza * 3)
+listin = [1, 2, 3, 4, 5, 6, 7, 8]
+listin.sort(reverse = True)
+print(listin)
+listillo = tuple(listin)
+print(listillo)
+print(listillo[1])
+print(4 in listillo)
+print(listillo.count(3))
+print()
 
-
-print("PRUEBA DE EDAD DE TIMOTHY")
-timmy = 0
-while timmy < 16:
-     print("rompela con timmy ", (timmy))
-     timmy += 1
-     if timmy > 15:
-        print("rupert")
-
-
-tic = 4
-tip = 8
-
-print("la variable tic es de tipo", type(tic))
-print("la variable tip es del tipo", type(tip))
-
-if(type(tic) == type(tip)):
-        print("la variable ", (tic),"y la variable ", (tip), "son del mismo tipo")
-else:
-        print("ambas variabes no son del mismo tipo")
-for t in range(1,21):
-        if(t % 2 == 0):
-                print("el numero", (t), "es par")
-        else:
-                print("el numero", (t), "es impar") 
-
-for g in range(0,6):
-        print((g), "a la tercera potencia es igual a", (g ** 3))
-r = 0       
-while r < tip+1:
-        print(r)
-        r += 1
-
-h = 4
-p = 1
-
-#for g in range(1,h+1):
-        #p = p * g
-#print(p)
-
-while h > 1:
-        p = p * h
-        h = h - 1
-print(p)
